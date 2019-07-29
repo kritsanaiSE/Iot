be9cb6dfd1aa01b3aa0ffb5d799fe4e5ee491efe
kritsanaiSE <31130280+kritsanaiSE@users.noreply.github.com>
2019-07-30 02:31:13 +0700

    turn on pin

---
+++ Turn_on_PIN.ino
@@ -0,0 +1,9 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(17, OUTPUT); //เรียกใช้งาน PIN และเลือกโหมด
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(17, HIGH); // สั่งให้ PIN ทำงาน HIGH=เปิด LOW=ปิด
+}
